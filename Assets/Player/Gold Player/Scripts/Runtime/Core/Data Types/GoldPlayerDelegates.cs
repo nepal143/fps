6f04565c8b2859a13dde53fcc1c144ad85b0be5c
nepla143 <nepalsss007@gmail.com>
2024-10-08 19:41:46 +0530

    finaly camera transform corrected

--- Assets/Player/Gold Player/Scripts/Runtime/Core/Data Types/GoldPlayerDelegates.cs
+++
@@ -1,9 +0,0 @@
-namespace Hertzole.GoldPlayer
-{
-    public static class GoldPlayerDelegates
-    {
-        public delegate void PlayerEvent();
-        public delegate void JumpEvent(float height);
-        public delegate void LandEvent(float fallHeight);
-    }
-}
